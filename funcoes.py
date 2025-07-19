30e290f4f42ecf74e43b2933e63a81df073b12ab
edson <edson.ferreira@alumni.usp.br>
2025-07-18 21:11:28 -0300

    funcoes

--- funcoes.py
+++ funcoes.py
@@ -5,17 +5,17 @@ import os
 import flet as ft
 
 dados = {
-    'Arquivo': os.listdir('assets'),
+    'Arquivo': os.listdir('assets/gifs'),
 }
 df_gifs = pd.DataFrame(dados)
 
 # with SessionLocal as db:
 def obter_gifs(id:int, db): 
     index = []
-    for item in db.query(Treino).filter_by(usuario_id=id):
-        for exercicio in item.exercicios:
-            print(exercicio.nome)
-            filtro = df_gifs['Arquivo'].str.contains(exercicio.nome, case=False, na=False)
+    for item in db.query(Treino).filter_by(usuario_id=id).all():
+        for exercicio in item.exercicios_prescritos:
+            # print(exercicio.nome)
+            filtro = df_gifs['Arquivo'].str.contains(exercicio.exercicio.nome, case=False, na=False)
             if not df_gifs[filtro].empty:
                 ids = df_gifs[filtro].index.tolist()
                 index.extend(ids)
@@ -25,6 +25,8 @@ def obter_gifs(id:int, db):
 # def criar_card(nome: str, descricao: str, imagem_url: str) -> ft.Card:
 def criar_card(nome: str, series: int, repeticoes:int, imagem_url: str, page) -> ft.Card:
     return ft.Card(
+        width=250,
+        expand=True, 
         col={"xs": 10, "sm": 6, "md": 4},
         # elevation=4,
         content=ft.Container(
@@ -32,13 +34,16 @@ def criar_card(nome: str, series: int, repeticoes:int, imagem_url: str, page) ->
             padding=10,
             content=ft.Column([
                 ft.Row(
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    # vertical_alignment=ft.MainAxisAlignment.CENTER,
                     expand=2,
                     # scroll='auto',
                     controls=[ft.Image(
                         src=imagem_url,
                         # width=page.width * 0.2,
                         # height=page.height * 0.2,
-                        fit=ft.ImageFit.CONTAIN
+                        fit=ft.ImageFit.CONTAIN, 
+                        expand=1, 
                         ),
                     ],
                     
@@ -48,7 +53,7 @@ def criar_card(nome: str, series: int, repeticoes:int, imagem_url: str, page) ->
                     alignment=ft.MainAxisAlignment.CENTER,
                     horizontal_alignment=ft.MainAxisAlignment.CENTER,
                     controls=[
-                        ft.Text(nome, style=ft.TextThemeStyle.TITLE_MEDIUM),
+                        ft.Text(nome.title(), style=ft.TextThemeStyle.TITLE_MEDIUM, no_wrap=False, max_lines=2),
                         ft.Text(f"Séries: {series}", size=12, italic=True),
                         ft.Text(f"Repetições: {repeticoes}", size=12, italic=True)
                     ],
